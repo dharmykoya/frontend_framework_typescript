9151864e459d66151f30f899ad1e4465916192a3
Damilola Adekoya <dharmyoya38x@gmail.com>
2021-04-15 09:38:46 +0100

    final update

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,34 +3,34 @@ import {Collection} from "./models/Collection";
 import {UserProps} from "./models/User";
 import {User} from "./models/User";
 import {UserList} from "./views/UserList";
-// import {UserEdit} from "./views/UserEdit";
-//
-//
-// const user = User.buildUser({name: 'Natash Oma', age: 39})
-// const root = document.getElementById('root')
-//
-// if(!root) {
-//     throw new Error("Root element not found")
-// }
-// const userEdit = new UserEdit(root, user)
-// //201
-//
-// userEdit.render()
-//
-// console.log(111, userEdit)
+import {UserEdit} from "./views/UserEdit";
 
 
-const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
-    return User.buildUser(json)
-})
+const user = User.buildUser({name: 'Natash Oma', age: 39})
+const root = document.getElementById('root')
 
-users.on('change', () => {
-    const root = document.getElementById('root')
+if(!root) {
+    throw new Error("Root element not found")
+}
+const userEdit = new UserEdit(root, user)
+//201
 
-    if(!root) {
-        throw new Error("Root element not found")
-    }
-    new UserList(root, users).render()
-})
+userEdit.render()
 
-users.fetch()
+console.log(111, userEdit)
+
+
+// const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+//     return User.buildUser(json)
+// })
+//
+// users.on('change', () => {
+//     const root = document.getElementById('root')
+//
+//     if(!root) {
+//         throw new Error("Root element not found")
+//     }
+//     new UserList(root, users).render()
+// })
+//
+// users.fetch()
